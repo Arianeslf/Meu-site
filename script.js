2a288f9917b82127444d385dcfa8a8df25aa740e
Ariane <arianesilvestremaira73@gmail.com>
2025-08-11 20:14:16 -0300

    modified script.js and style.css in .

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-let lastScroll = 0;
-const topBar = document.querySelector(".top-bar");
-
-window.addEventListener("scroll", () => {
-  let currentScroll = window.pageYOffset;
-
-  if (currentScroll > lastScroll) {
-    // rolando para baixo → esconder
-    topBar.classList.add("hidden");
-  } else {
-    // rolando para cima → mostrar
-    topBar.classList.remove("hidden");
+window.addEventListener("resize", () => {
+  const infoFlex = document.querySelector(".info-flex");
+  if (infoFlex) {
+    infoFlex.scrollLeft = 0;
   }
-
-  lastScroll = currentScroll;
 });
